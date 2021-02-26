b375f2deb77aa5355a1b5df5e51a53722cfa6484
dongqunren <chenjunrenyx@163.com>
2021-02-26 23:42:02 +0800

    添加测试示例

--- my_java/src/test/java/web_test/Base.java
+++ my_java/src/test/java/web_test/Base.java
@@ -1,13 +1,16 @@
 package web_test;
 
+import org.openqa.selenium.UnexpectedAlertBehaviour;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.ie.InternetExplorerOptions;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -24,6 +27,7 @@ public class Base {
 
             DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
             desiredCapabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+            desiredCapabilities.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.IGNORE);
             desiredCapabilities.setCapability(InternetExplorerDriver.INITIAL_BROWSER_URL, "http://www.baidu.com");
             InternetExplorerOptions internetExplorerOptions = new InternetExplorerOptions();
             internetExplorerOptions.merge(desiredCapabilities);
@@ -33,25 +37,33 @@ public class Base {
             System.setProperty("webdriver.firefox.bin", "C:\\Program Files\\Mozilla Firefox\\firefox.exe");
             System.setProperty("webdriver.gecko.driver", "C:\\webdriver\\geckodriver.exe");
 
-            driver = new FirefoxDriver();
+            DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+            desiredCapabilities.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.IGNORE);
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            firefoxOptions.merge(desiredCapabilities);
+
+            driver = new FirefoxDriver(firefoxOptions);
         } else if ("chrome".equalsIgnoreCase(browser)) {
             System.setProperty("webdriver.chrome.driver", "C:\\webdriver\\chromedriver.exe");
 
-            driver = new ChromeDriver();
+            DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+            desiredCapabilities.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.IGNORE);
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.merge(desiredCapabilities);
+
+            driver = new ChromeDriver(chromeOptions);
         } else {
             System.out.println("browser: " + browser + ", 暂不支持。");
         }
+    }
 
+    @AfterSuite
+    public void close() {
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        
-    }
-
-    @AfterSuite
-    public void close() {
         driver.quit();
     }
 }
